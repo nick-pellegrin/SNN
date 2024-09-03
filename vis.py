1e02f396088e8949adf3738f4e2757800fa55450
Nick Pellegrin <nicholaspellegrin@yahoo.com>
2024-09-03 12:09:32 -0700

    fixed raster plots

--- vis.py
+++ vis.py
@@ -3,17 +3,17 @@ from snntorch import spikegen
 import snntorch.spikeplot as splt
 import matplotlib.pyplot as plt
 
-spike_data = spikegen.rate_conv(torch.rand((200, 784))).unsqueeze(1)
+spike_in = spikegen.rate_conv(torch.rand((200, 784))).unsqueeze(1)
 # sample contains 784 neurons, and each neuron has 200 time steps
-print(spike_data.size())
 # >>> torch.Size([200, 1, 784])
+
+spike_data = spike_in.squeeze(1)
+print(spike_data.shape)
 
 
 fig = plt.figure(facecolor="w", figsize=(10, 5))
 ax = fig.add_subplot(111)
-
-#  s: size of scatter points; c: color of scatter points
-splt.raster(spike_data, ax, s=1.5, c="black")
+splt.raster(spike_data, ax, s=1.5, c="black", marker='|', linewidths=0.5)
 plt.title("Input Layer")
 plt.xlabel("Time step")
 plt.ylabel("Neuron Number")
