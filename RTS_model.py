8c34f1e4f221ef7e9175849ffd0e803234b9d3ea
Nick Pellegrin <nicholaspellegrin@yahoo.com>
2024-09-04 18:27:52 -0700

    Added training for conv model

--- RTS_model.py
+++ RTS_model.py
@@ -6,20 +6,58 @@ date: 10/21/2021
 # IMPORTS --------------------------------------------------------------------------------------------
 import torch
 import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+import torch.nn.functional as F
+
 import snntorch as snn
 from snntorch import spikeplot as splt
 from snntorch import spikegen
+from snntorch import surrogate
+from snntorch import backprop
+from snntorch import functional as SF
+from snntorch import utils
+
 import matplotlib.pyplot as plt
+import numpy as np
+import itertools
+
+
+
+
+
+# DATA LOADER -----------------------------------------------------------------------------------------
+batch_size = 128
+data_path = '/tmp/data/mnist'
+
+dtype = torch.float
+device = torch.device("cpu")
+
+transform = transforms.Compose([
+            transforms.Resize((28, 28)),
+            transforms.Grayscale(),
+            transforms.ToTensor(),
+            transforms.Normalize((0,), (1,))])
+
+mnist_train = datasets.MNIST(data_path, train=True, download=True, transform=transform)
+mnist_test = datasets.MNIST(data_path, train=False, download=True, transform=transform)
+
+train_loader = DataLoader(mnist_train, batch_size=batch_size, shuffle=True, drop_last=True)
+test_loader = DataLoader(mnist_test, batch_size=batch_size, shuffle=True, drop_last=True)  
+
+
 
 
 
 # NEURON PARAMETERS ----------------------------------------------------------------------------------
 beta = 0.85
+spike_grad = surrogate.fast_sigmoid(slope=25)
+
 
 # NETWORK HYPERPARAMETERS ----------------------------------------------------------------------------
-num_inputs = 784
-num_hidden = 1000
-num_outputs = 10
+# num_inputs = 784
+# num_hidden = 1000
+# num_outputs = 10
 
 
 # NETWORK DEFINITION ---------------------------------------------------------------------------------
@@ -28,47 +66,148 @@ class Net(nn.Module):
         super().__init__()
 
         # initialize layers
-        self.fc1 = nn.Linear(num_inputs, num_hidden)
-        self.lif1 = snn.Leaky(beta=beta)
-        self.fc2 = nn.Linear(num_hidden, num_outputs)
-        self.lif2 = snn.Leaky(beta=beta)
+        self.conv1 = nn.Conv2d(1, 12, kernel_size=5, stride=1)
+        self.lif1 = snn.Leaky(beta=beta, spike_grad=spike_grad)
+        self.conv2 = nn.Conv2d(12, 64, kernel_size=5, stride=1)
+        self.lif2 = snn.Leaky(beta=beta, spike_grad=spike_grad)
+        self.fc1 = nn.Linear(64*4*4, 10)
+        self.lif3 = snn.Leaky(beta=beta, spike_grad=spike_grad)
 
-    def step(self, spk_inputs, mem_potentials):
-        cur1 = self.fc1(spk_inputs)
-        spk1, mem_potentials[0] = self.lif1(cur1, mem_potentials[0])
-        cur2 = self.fc2(spk1)
-        spk2, mem_potentials[1] = self.lif2(cur2, mem_potentials[1])
-        return spk2
 
-    def simulate(self, num_steps):
+    def forward(self, x):
         mem1 = self.lif1.init_leaky()
         mem2 = self.lif2.init_leaky()
-        mem_potentials = [mem1, mem2]
+        mem3 = self.lif3.init_leaky()
 
-        out_rec = []
+        cur1 = F.max_pool2d(self.conv1(x), 2)
+        spk1, mem1 = self.lif1(cur1, mem1)
 
-        for step in range(num_steps):
-            spk_in = spikegen.rate_conv(torch.rand((1, num_inputs))).unsqueeze(1)
-            spk_out = self.step(spk_in, mem_potentials)
-            out_rec.append(spk_out)
+        cur2 = F.max_pool2d(self.conv2(spk1), 2)
+        spk2, mem2 = self.lif2(cur2, mem2)
+
+        cur3 = self.fc1(spk2.view(batch_size, -1))
+        spk3, mem3 = self.lif3(cur3, mem3)
+
+        return spk3, mem3
+
+
+    
+    # def step(self, spk_inputs, mem_potentials):
+    #     cur1 = self.fc1(spk_inputs)
+    #     spk1, mem_potentials[0] = self.lif1(cur1, mem_potentials[0])
+    #     cur2 = self.fc2(spk1)
+    #     spk2, mem_potentials[1] = self.lif2(cur2, mem_potentials[1])
+    #     return spk2
+
+    # def simulate(self, num_steps):
+    #     mem1 = self.lif1.init_leaky()
+    #     mem2 = self.lif2.init_leaky()
+    #     mem_potentials = [mem1, mem2]
+
+    #     out_rec = []
+
+    #     for step in range(num_steps):
+    #         spk_in = spikegen.rate_conv(torch.rand((1, num_inputs))).unsqueeze(1)
+    #         spk_out = self.step(spk_in, mem_potentials)
+    #         out_rec.append(spk_out)
         
-        return torch.stack(out_rec)
+    #     return torch.stack(out_rec)
 
+
+
+
+
+# TRAINING --------------------------------------------------------------------------------------------
+def forward_pass(net, num_steps, data):
+    mem_rec = []
+    spk_rec = []
+    utils.reset_net(net)
+
+    for step in range(num_steps):
+        spk_out, mem_out = net(data)
+        spk_rec.append(spk_out)
+        mem_rec.append(mem_out)
+
+    return torch.stack(spk_rec), torch.stack(mem_rec)
+
+
+def batch_accuracy(train_loader, net, num_steps):
+    with torch.no_grad():
+        total = 0
+        acc = 0
+        net.eval()
+
+        train_loader = iter(train_loader)
+        for data, targets in train_loader:
+            data = data.to(device)
+            targets = targets.to(device)
+            spk_rec, _ = forward_pass(net, num_steps, data)
+
+            acc += SF.accuracy_rate(spk_rec, targets) * spk_rec.size(1)
+            total += spk_rec.size(1)
+
+    return acc/total
+
+
+def train(net, train_loader, num_steps, optimizer):
+    optimizer = torch.optim.Adam(net.parameters(), lr=1e-2, betas=(0.9, 0.999))
+    loss_fn = SF.ce_rate_loss()
+    num_epochs = 1
+    loss_hist = []
+    test_acc_hist = []
+    counter = 0
+
+    # Outer training loop
+    for epoch in range(num_epochs):
+
+        # Training loop
+        for data, targets in iter(train_loader):
+            data = data.to(device)
+            targets = targets.to(device)
+
+            # forward pass
+            net.train()
+            spk_rec, _ = forward_pass(net, num_steps, data)
+
+            # initialize the loss & sum over time
+            loss_val = loss_fn(spk_rec, targets)
+
+            # Gradient calculation + weight update
+            optimizer.zero_grad()
+            loss_val.backward()
+            optimizer.step()
+
+            # Store loss history for future plotting
+            loss_hist.append(loss_val.item())
+
+            # Test set
+            if counter % 50 == 0:
+                with torch.no_grad():
+                    net.eval()
+
+                    # Test set forward pass
+                    test_acc = batch_accuracy(test_loader, net, num_steps)
+                    print(f"Iteration {counter}, Test Acc: {test_acc * 100:.2f}%\n")
+                    test_acc_hist.append(test_acc.item())
+
+            counter += 1
 
 
 # MAIN ------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     net = Net()
+    
 
-    output = net.simulate(200)
-    print(output.shape)
-    output = output.squeeze(1)
-    output = output.squeeze(1)
-    print(output.shape)
 
-    fig = plt.figure(facecolor="w", figsize=(10, 5))
-    ax = fig.add_subplot(111)
-    splt.raster(output, ax, s=100, c="black", marker='|', linewidths=2.5)
-    plt.title("Output Layer")
-    plt.xlabel("Time step")
-    plt.show()
+    # output = net.simulate(200)
+    # print(output.shape)
+    # output = output.squeeze(1)
+    # output = output.squeeze(1)
+    # print(output.shape)
+
+    # fig = plt.figure(facecolor="w", figsize=(10, 5))
+    # ax = fig.add_subplot(111)
+    # splt.raster(output, ax, s=100, c="black", marker='|', linewidths=2.5)
+    # plt.title("Output Layer")
+    # plt.xlabel("Time step")
+    # plt.show()
